0e7179f2b8d83d16e95942c9c037f27589e9d3f1
Karan <karantank@Karans-MacBook-Pro>
2026-04-19 00:48:57 -0400

    fix lint

--- src/shared/components/RichTextDisplay.tsx
+++ src/shared/components/RichTextDisplay.tsx
@@ -10,7 +10,6 @@ export default function RichTextDisplay({ html, className = '' }: RichTextDispla
   return (
     <div
       className={`prose prose-sm max-w-none [&_ul]:list-disc [&_ul]:pl-5 [&_ol]:list-decimal [&_ol]:pl-5 [&_a]:text-blue-600 [&_a]:underline ${className}`}
-      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: clean }}
     />
   );
